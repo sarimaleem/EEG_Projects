592c0682cc494d39ebfced879909d4015d24beff
sarimaleem <34404894+sarimaleem@users.noreply.github.com>
2018-07-30 08:53:36 -0500

    Delete sumOfBandsPercentage.m

--- sumOfBandsPercentage.m
+++
@@ -1,8 +0,0 @@
-function percentage = sumOfBandsPercentage(sumOfBandPowers)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-totalSum = sum(sum(sumOfBandPowers));
-rowSum = sum(sumOfBandPowers, 2);
-percentage = rowSum/totalSum*100;
-
-end
